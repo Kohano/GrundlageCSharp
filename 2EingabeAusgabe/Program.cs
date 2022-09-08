7b539310bd6c46d6e27424adb22b6f0844d3152f
Sumiyabazar Gerelt Od <sumiyabaza.g@gso.schule.koeln>
2022-09-08 11:19:45 +0200

    Finished version

--- 2EingabeAusgabe/Program.cs
+++ 2EingabeAusgabe/Program.cs
@@ -1,2 +1,65 @@
 ﻿// A1
-Console.WriteLine("Hello!");
+using System;
+namespace ConsoleOutput
+{
+    internal class Programm
+    {
+        static void Main()
+        {
+            Console.WriteLine(" .-.\n(o o)\n| O |\n|   |\n'~~~' ");
+
+        }
+    }
+}
+
+
+
+
+//A2
+     using System; 
+    namespace ConsoleOutput
+{
+
+    internal class Program
+    {
+
+        static void Main()
+        {
+
+            string firstname = "Sumiyabazar";
+            string Lastname = "Gerelt-Od";
+            int age = 20;
+            Console.WriteLine(firstname + "\n" + Lastname + "\n" + age);
+
+        }
+    }
+}
+
+//A3
+using System;
+ 
+namespace ConsoleOutput
+{
+
+    internal class Program
+    {
+
+        static void Main()
+        {
+
+            Console.WriteLine("What is your name?");
+
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine(FirstName);
+
+            Console.WriteLine("What is your Lastname?");
+
+            string Lastname = Console.ReadLine();
+
+            string age = Console.ReadLine();
+
+
+        }
+    }
+}
