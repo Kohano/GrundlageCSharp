be7a60246578781597ee9f07e8c5bf260df8e7b5
Sumiyabazar Gerelt Od <sumiyabaza.g@gso.schule.koeln>
2022-09-11 10:54:27 +0200

    final ver

--- 3DatenTypenKonvertierung/Program.cs
+++ 3DatenTypenKonvertierung/Program.cs
@@ -1,9 +1,34 @@
-﻿Console.WriteLine($"size of short ={sizeof(short)}");
+﻿using System;
+
+Console.WriteLine($"size of short ={sizeof(short)}");
 Console.WriteLine($"size of short ={sizeof(short)}");
 Console.WriteLine($"nint.MinValue = {short.MinValue}");
 Console.WriteLine($"nint.MaxValue = {short.MaxValue}");
 
+Console.WriteLine($"size of int ={sizeof(int)}");
+Console.WriteLine($"size of int ={sizeof(int)}");
+Console.WriteLine($"int.MinValue = {int.MinValue}");
+Console.WriteLine($"int.MaxValue = {int.MaxValue}");
 
+Console.WriteLine($"size of long ={sizeof(long)}");
+Console.WriteLine($"size of long ={sizeof(long)}");
+Console.WriteLine($"long.MinValue = {long.MinValue}");
+Console.WriteLine($"long.MaxValue = {long.MaxValue}");
+
+Console.WriteLine($"size of float ={sizeof(float)}");
+Console.WriteLine($"size of float ={sizeof(float)}");
+Console.WriteLine($"float.Minvalue = {float.MinValue}");
+Console.WriteLine($"float.Maxvalue = {float.MaxValue}");
+
+Console.WriteLine($"size of double ={sizeof(double)}");
+Console.WriteLine($"size of double ={sizeof(double)}");
+Console.WriteLine($"double.Minvalue = {double.MinValue}");
+Console.WriteLine($"double.Maxvalue = {double.MaxValue}");
+
+Console.WriteLine($"size of decimal ={sizeof(decimal)}");
+Console.WriteLine($"size of decimal ={sizeof(decimal)}");
+Console.WriteLine($"decimal.Minvalue = {decimal.MinValue}");
+Console.WriteLine($"decimal.Maxvalue = {decimal.MaxValue}");
 
 //A2
 {
